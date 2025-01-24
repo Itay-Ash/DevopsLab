e8489e48da4324df3826094e2de3f155b0289aa3
OwwFire <itayash1002@gmail.com>
2025-01-24 22:09:08 +0200

    Created Ansible inventory!

---
+++ ansible/gcp-ssh-wraper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This is a wrapper script allowing to use GCP's IAP SSH option to connect
+# to our servers.
+
+# Ansible passes a large number of SSH parameters along with the hostname as the
+# second to last argument and the command as the last. We will pop the last two
+# arguments off of the list and then pass all of the other SSH flags through
+# without modification:
+cmd="${@: -1: 1}" #Grabs the last argument sent
+host="${@: -2: 1}" #Grabs the second to last argument
+
+# Unfortunately ansible has hardcoded ssh options, so we need to filter these out.
+# It's an ugly hack, but for now we'll only accept the options starting with '--'.
+declare -a opts 
+for ssh_arg in "${@: 1: $# -3}" ; do #Gathers all options and filters out options that don't start with -- so only custom added gcloud commands would stay
+    if [[ "${ssh_arg}" == --* ]] ; then
+        opts+=("${ssh_arg}")
+    fi
+done
+
+# Execute the gcloud compute ssh command with filtered options
+exec gcloud compute ssh "${opts[@]}" "${host}" -- -C "${cmd}"
