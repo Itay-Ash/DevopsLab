42d5ba8c2dbefa468c445c080f6f193315486703
OwwFire <itayash1002@gmail.com>
2025-04-10 10:40:29 +0300

    Commented a way to maintain a consistent ip address even after running terraform destroy

--- terraform/network.tf
+++ terraform/network.tf
@@ -55,6 +55,12 @@ resource "google_compute_address" "web_server_public_ip" {
   name    = "web-server-public-ip"
   region  = var.region
   description = "Static public IP for the web server"
+
+# In order to maintain a consistent IP address this will need to be added.
+# Because this is a lab that is consistently destroyed and rebuilt I commented it out.
+#  lifecycle {
+#    prevent_destroy = true
+#  }
 }
 
 resource "google_compute_address" "jenkins_server_public_ip" {
