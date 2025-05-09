6d0855fdad420fc8e919424bdc11e48ee6735b80
Itay-Ash <itayash1002@gmail.com>
2025-05-09 17:24:28 +0300

    Added usage of fixture

--- code/Web/backend/tests/unit/test_main_unit.py
+++ code/Web/backend/tests/unit/test_main_unit.py
@@ -2,8 +2,10 @@ import pytest
 import main
 from exceptions.env_exceptions import MissingEnvVariableError
 
+@pytest.fixture
+def mock_env_var_del(monkeypatch):
+    monkeypatch.delenv("API_PORT")
 
-def test_load_api_env_var(monkeypatch):
+def test_load_api_env_var(mock_env_var_del):
     with pytest.raises(MissingEnvVariableError):   
-        monkeypatch.delenv("API_PORT")
         main.load_api_env_var()
