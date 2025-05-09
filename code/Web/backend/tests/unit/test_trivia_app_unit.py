4b372e24b0ded2e044d365fc28fe75e44c95ac96
Itay-Ash <itayash1002@gmail.com>
2025-05-09 17:23:41 +0300

    Optimized code

--- code/Web/backend/tests/unit/test_trivia_app_unit.py
+++ code/Web/backend/tests/unit/test_trivia_app_unit.py
@@ -1,10 +1,12 @@
 import pytest
 from exceptions.env_exceptions import MissingEnvVariableError
 
-def test_load_env_vars(monkeypatch):
+@pytest.fixture
+def mock_env_var_del(monkeypatch):
+    monkeypatch.delenv("DB_USERNAME", raising=False)
+
+def test_load_env_vars(mock_env_var_del):
     with pytest.raises(MissingEnvVariableError):   
-        # Runs load_env_vars on file load.
-        # Excpects MissingEnvVariableError error.
-        monkeypatch.delenv("DB_USERNAME", raising=False)
+        # Runs load_env_vars on file load if ran individually.
         from app.trivia_app import load_env_vars
         load_env_vars()
