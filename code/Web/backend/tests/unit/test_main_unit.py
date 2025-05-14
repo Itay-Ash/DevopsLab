6f04110df30982a9520bc2de58b455f17c187255
Itay-Ash <itayash1002@gmail.com>
2025-05-14 15:23:51 +0300

    Specified importing inside unit tests

--- code/Web/backend/tests/unit/test_main_unit.py
+++ code/Web/backend/tests/unit/test_main_unit.py
@@ -1,11 +1,11 @@
 import pytest
-import main
+from main import load_api_env_var
 from exceptions.env_exceptions import MissingEnvVariableError
 
 @pytest.fixture
 def mock_env_var_del(monkeypatch):
-    monkeypatch.delenv("API_PORT")
+    monkeypatch.delenv("API_PORT", raising=False)
 
 def test_load_api_env_var(mock_env_var_del):
     with pytest.raises(MissingEnvVariableError):   
-        main.load_api_env_var()
+        load_api_env_var()
