00f98bd61490bfa277e34da92dcfe4338cfb502d
Itay-Ash <itayash1002@gmail.com>
2025-06-20 23:25:10 +0300

    Improved agent configuration

--- terraform/scripts/docker-agent-vm-startup-script.sh
+++ terraform/scripts/docker-agent-vm-startup-script.sh
@@ -1,5 +1,6 @@
 if ! command -v docker &>/dev/null; then
-  apt-get update && apt-get install -y ca-certificates curl git openjdk-17-jdk openjdk-17-jre
+  apt-get update && apt-get install -y openjdk-17-jre
+  apt-get install -y ca-certificates curl 
   install -m 0755 -d /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
   chmod a+r /etc/apt/keyrings/docker.asc
@@ -7,5 +8,5 @@ if ! command -v docker &>/dev/null; then
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
     xenial stable" | \
     tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt-get update && apt-get install -y docker-ce 
+    apt-get update && apt-get install -y docker-ce git
 fi
